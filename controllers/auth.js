aa6610e8fa7b0bf644de02f4315807b4e464a2a1
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-10 13:57:53 +0700

    Finishing login and signup authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,15 +17,36 @@ const signup = async (req, res) => {
         
         const hashedPasswaord = await bcrypto.hash(password, 10);
 
+        const token = serverClient.createUserToke(userId);
+
+        res.status(200).json({ token, fullName, username, userId, hashedPasswaord, phoneNumber });
+
     } catch (error) {
         console.log(error);
 
         resizeBy.status(500).json({ message: error });
     }
 };
-const login = (req, res) => {
+const login = async (req, res) => {
     try {
+        const { username, password } = req.body;
 
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ name: username});
+
+        if(!users.length) return res.status(400).json({ message: 'User not found' });
+
+        const success = await bcrypto.compare(password, users[0].hashedPasswaord);
+
+        const token = serverClient.createUserToke(users[0].id);
+
+        if(success){
+            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
+        } else {
+            res.status(500).json({ message: 'Incorrect password'});
+        }
     } catch (error) {
         console.log(error);
 
